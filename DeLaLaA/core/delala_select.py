f5286d98477ba013ab763eae0ecb6f2e534036c0
Ji Xu (徐计) <alanxuch@hotmail.com>
2024-03-10 19:10:06 +0800

    Replace the folders
    Replace the misplaced folders

---
+++ DeLaLaA/core/delala_select.py
@@ -0,0 +1,201 @@
+import numpy as np
+
+
+def DeLaLA_select(gamma, rho, layer, Y, k, l, rootWeight):
+    """
+    select the samples to label according to Objective Function
+    :param gamma: center potential
+    :param rho: local density
+    :param layer: layer index of each sample
+    :param alhpa: parameter of the divergence item
+    :param Y: labels
+    :param k: k for LMCA
+    :param l: given number of samples to be labeled
+    :return: labeled samples Indics
+    """
+    N = len(Y)
+    psi = np.zeros(N, dtype=float)
+    np.divide(rho, layer, psi)
+    C = len(np.unique(Y))
+    label_unique = np.unique(Y)
+
+    y = [x for x in Y]
+
+    for i in range(len(y)):
+        for j in range(C):
+            if y[i] == label_unique[j]:
+                y[i] = j
+    LabeledPerClass = np.zeros((C, k), dtype=int) - 1
+    p = l - C * k  ## number of global selection
+    globalSelected = np.zeros(p, dtype=int) - 1
+    gamma = (gamma - min(gamma)) / (max(gamma) - min(gamma))
+    BigValue = 10
+    hgamma = np.zeros(N, dtype=float)
+    for i in range(N):
+        if np.abs(gamma[i] - 1) < 1E-3:
+            hgamma[i] = BigValue
+        else:
+            if np.abs(gamma[i]) < 1E-3:
+                hgamma[i] = 0
+            else:
+                hgamma[i] = 1.0 / np.log(gamma[i])
+
+    sortedInds = SortSmallXOR(hgamma, psi, rootWeight)
+    Cursor = 0
+    # hgammaCursor =0
+    labeled = 0
+    while labeled < l:
+        classID = y[sortedInds[Cursor]]
+        if LabeledPerClass[classID, k - 1] > -1:  ### selection has done for the class.
+            if p > 0:
+                if globalSelected[p - 1] > -1:  ### global selection done
+                    Cursor += 1
+                    continue
+                else:
+                    for i in range(p):
+                        if globalSelected[i] == -1:
+                            globalSelected[i] = sortedInds[Cursor]  ###selected one sample in global selection
+                            labeled += 1
+                            Cursor += 1
+                            break
+            else:
+                Cursor += 1
+        else:
+            for i in range(k):
+                if LabeledPerClass[classID, i] == -1:
+                    LabeledPerClass[classID, i] = sortedInds[Cursor]  ###selected one sample for a given class
+                    labeled += 1
+                    Cursor += 1
+                    break
+    result = np.append(LabeledPerClass.flatten(), globalSelected)
+    return result
+
+def Fuzzy_select(gamma, rho, layer, Y, k, l, rootWeight):
+    """
+    select the samples to label according to Objective Function
+    :param gamma: center potential
+    :param rho: local density
+    :param layer: layer index of each sample
+    :param alhpa: parameter of the divergence item
+    :param Y: labels
+    :param k: k for LMCA
+    :param l: given number of samples to be labeled
+    :return: labeled samples Indics
+    """
+    N = len(Y)
+    psi = np.zeros(N, dtype=float)
+    ###revised, reversion
+    np.divide(layer, rho,  psi)
+    psi = np.log(psi)
+    ### added log
+
+    C = len(np.unique(Y))
+    label_unique = np.unique(Y)
+
+    y = [x for x in Y]
+
+    for i in range(len(y)):
+        for j in range(C):
+            if y[i] == label_unique[j]:
+                y[i] = j
+    LabeledPerClass = np.zeros((C, k), dtype=int) - 1
+    p = l - C * k  ## number of global selection
+    globalSelected = np.zeros(p, dtype=int) - 1
+    #gamma = (gamma - min(gamma)) / (max(gamma) - min(gamma))
+    SMALLValue = 0.00001
+    hgamma = np.zeros(N, dtype=float)
+    for i in range(N):
+        if gamma[i] < 1E-5:
+            gamma[i] = SMALLValue
+    #     else:
+    #         if np.abs(gamma[i]) < 1E-3:
+    #             hgamma[i] = 0
+    #         else:
+    #             #hgamma[i] = 1.0 / np.log(gamma[i])
+    #             hgamma[i] = np.log(gamma[i])
+    hgamma = np.log(gamma)
+    #hgamma = gamma
+    sortedInds = SortSmallXOR2(hgamma, psi, rootWeight)
+    Cursor = 0
+    # hgammaCursor =0
+    labeled = 0
+    while labeled < l:
+        classID = y[sortedInds[Cursor]]
+        if LabeledPerClass[classID, k - 1] > -1:  ### selection has done for the class.
+            if p > 0:
+                if globalSelected[p - 1] > -1:  ### global selection done
+                    Cursor += 1
+                    continue
+                else:
+                    for i in range(p):
+                        if globalSelected[i] == -1:
+                            globalSelected[i] = sortedInds[Cursor]  ###selected one sample in global selection
+                            labeled += 1
+                            Cursor += 1
+                            break
+            else:
+                Cursor += 1
+        else:
+            for i in range(k):
+                if LabeledPerClass[classID, i] == -1:
+                    LabeledPerClass[classID, i] = sortedInds[Cursor]  ###selected one sample for a given class
+                    labeled += 1
+                    Cursor += 1
+                    break
+    result = np.append(LabeledPerClass.flatten(), globalSelected)
+    return result
+
+def SortSmallXOR2(a, b, rootWeight=0.5):
+    '''
+    a and b are exclusively small. The function sort the values of a XOR b in the sense of being small
+    :param a:
+    :param b:
+    :param rootWeight: if >0, tends to include more roots than divergent samples
+    :return: the indics of an array, first elements are small in a or small in b.
+    '''
+
+    ###select via XOR. min-max normalization failed
+    # a = (a - np.mean(a)) / np.std(a)
+    # b = (b - np.mean(b)) / np.std(b)
+
+    normalA = (a - min(a)) / (max(a) - min(a))
+    normalB = (b - min(b)) / (max(b) - min(b))
+
+    ratio = np.mean(normalA) / np.mean(normalB)
+
+    print ("A to B ratio: ", np.mean(normalA)/np.mean(normalB))
+
+    Threshold = 2
+    if ratio>Threshold or ratio<1/Threshold:
+        normalB = normalB*ratio
+
+    y = rootWeight * normalA * (1 - normalB) + (1 - rootWeight) * normalB * (1 - normalA)
+    inds = np.argsort(y)
+    # print('Inds:',inds)
+    # ysort = np.sort(y)
+    # print('ysort:',ysort)
+    # result = inds[::-1] ###revised
+    return inds
+def SortSmallXOR(a, b, rootWeight=0.5):
+    '''
+    a and b are exclusively small. The function sort the values of a XOR b in the sense of being small
+    :param a:
+    :param b:
+    :param rootWeight: if >0, tends to include more roots than divergent samples
+    :return: the indics of an array, first elements are small in a or small in b.
+    '''
+
+    ###select via XOR. min-max normalization failed
+    normalA = (a - np.mean(a)) / np.std(a)
+    normalB = (b - np.mean(b)) / np.std(b)
+
+    if np.mean(normalB)!=0:
+        print ("A to B ratio: ", np.mean(normalA)/np.mean(normalB))
+
+    y = rootWeight * normalA * (1 - normalB) + (1 - rootWeight) * normalB * (1 - normalA)
+    inds = np.argsort(y)
+    # print('Inds:',inds)
+    # ysort = np.sort(y)
+    # print('ysort:',ysort)
+    result = inds[::-1] ###revised
+    return result
---
+++ DeLaLaA/core/leading_tree.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+
+class LeadingTree:
+    """
+    Leading Tree
+    """
+
+    def __init__(self, X_train, dc, lt_num, D):
+        self.X_train = X_train
+        self.dc = dc
+        self.lt_num = lt_num
+        self.D = D  # Calculate the distance matrix D
+        # print(f'The data type of the distance matrix D is {self.D.dtype}')
+        self.density = None
+        self.Pa = None
+        self.delta = None
+        self.gamma = None
+        self.gamma_D = None
+        self.Q = None
+        self.AL = [np.zeros((0, 1), dtype=int) for i in range(lt_num)]  # AL[i] store all indexes of a subtree
+        self.layer = np.zeros(len(X_train), dtype=int)
+
+    def ComputeLocalDensity(self, D, dc):
+        """
+        Calculate the local density of samples
+        :param D: The Euclidean distance of all samples
+        :param dc:Bandwidth parameters
+        :return:
+        self.density: local density of all samples
+        self.Q: Sort the density index in descending order
+        """
+        tempMat1 = np.exp(-(D ** 2))
+        tempMat = np.power(tempMat1, dc ** (-2))
+        self.density = np.sum(tempMat, 1, dtype='float32') - 1
+        self.Q = np.argsort(self.density)[::-1]
+
+        # print(f'The data type of density is {self.density.dtype}\n'  #       f'The data type of Q is {self.Q.dtype}')
+
+    def ComputeParentNode(self, D, Q):
+        """
+        Calculate the distance to the nearest data point of higher density (delta) and the parent node (Pa)
+        :param D: The Euclidean distance of all samples
+        :param Q:Sort by index in descending order of sample local density
+        :return:
+        self.delta: the distance of the sample to the closest data point with a higher density
+        self.Pa: the index of the parent node of the sample
+        """
+
+        self.delta = np.zeros(len(Q), dtype='float32')
+        self.Pa = np.zeros(len(Q), dtype=int)
+        for i in range(len(Q)):
+            if i == 0:
+                self.delta[Q[i]] = max(D[Q[i]])
+                self.Pa[Q[i]] = -1
+            else:
+                greaterInds = Q[0:i]
+                D_A = D[Q[i], greaterInds]
+                self.delta[Q[i]] = min(D_A)
+                self.Pa[Q[i]] = greaterInds[np.argmin(D_A)]
+
+        # print(f'The data type of delta is {self.delta.dtype}')
+
+    def ProCenter(self, density, delta, Pa):
+        """
+        Calculate the probability of being chosen as the center node and Disconnect the Leading Tree
+        :param density: local density of all samples
+        :param delta: the distance of the sample to the closest data point with a higher density
+        :param Pa: the index of the parent node of the sample
+        :return:
+        self.gamma: the probability of the sample being chosen as a center node
+        self.gamma_D: Sort the gamma index in descending order
+        """
+        self.gamma = density * delta
+        self.gamma_D = np.argsort(self.gamma)[::-1]
+        # print(f'The data type of gamma is {self.gamma.dtype}')
+        # Disconnect the Leading Tree
+        for i in range(self.lt_num):
+            Pa[self.gamma_D[i]] = -1
+
+    def GetSubtreeR(self, gamma_D, lt_num, Q, pa):
+        """
+         Subtree
+        :param gamma_D:
+        :param lt_num: the number of subtrees
+        :return:
+        self.AL: AL[i] store indexes of a subtrees, i = {0, 1, ..., lt_num-1}
+        """
+        for i in range(lt_num):
+            self.AL[i] = np.append(self.AL[i], gamma_D[i])
+
+        N = len(gamma_D)
+        treeID = np.zeros((N, 1), dtype=int) - 1
+        for i in range(lt_num):
+            treeID[gamma_D[i]] = i
+
+        for nodei in range(N):  ### casscade label assignment
+            curInd = Q[nodei]
+            if treeID[curInd] > -1:
+                continue
+
+            else:
+                paID = pa[curInd]
+                self.layer[curInd] = self.layer[paID] + 1
+                curTreeID = treeID[paID]
+                treeID[curInd] = curTreeID
+                self.AL[curTreeID[0]] = np.append(self.AL[curTreeID[0]], curInd)
+
+    def Edges(self, Pa):  # store edges of subtrees
+        """
+
+        :param Pa:  the index of the parent node of the sample
+        :return:
+        self. edges: pairs of child node and parent node
+        """
+        edgesO = np.array(list(zip(range(len(Pa)), Pa)))
+        ind = edgesO[:, 1] > -1
+        self.edges = edgesO[ind,]
+
+    def fit(self):
+        self.ComputeLocalDensity(self.D, self.dc)
+        self.ComputeParentNode(self.D, self.Q)
+        self.ProCenter(self.density, self.delta, self.Pa)
+        self.GetSubtreeR(self.gamma_D, self.lt_num, self.Q, self.Pa)
+        self.Edges(self.Pa)
+        self.layer = self.layer + 1
